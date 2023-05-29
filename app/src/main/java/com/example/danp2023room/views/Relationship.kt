cdc9a94be0c49bdc44a457a8967596ade4ff8edf
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-05-29 09:00:44 -0500

    ADD: Students and Courses UI

--- app/src/main/java/com/example/danp2023room/views/Relationship.kt
+++ app/src/main/java/com/example/danp2023room/views/Relationship.kt
@@ -1,19 +1,66 @@
 package com.example.danp2023room.views
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.danp2023room.classes.Destination
 
 @Composable
 fun Relationship(navController: NavHostController) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Relationship")
-        Button(onClick = { navController.navigate(Destination.RoomSample.route) }) {
+    val coursesWithStudent = MutableList(10) { it }
+
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(24.dp, 32.dp),
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text(
+                text = "Courses with Students",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn(
+            contentPadding = PaddingValues(all = 4.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(coursesWithStudent) {
+                Row {
+                    Column {
+                        Text("Course Name")
+                        Text("Student 1, Student 2, Student 3")
+                    }
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { navController.navigate(Destination.RoomSample.route) }) {
             Text(text = "Go Home")
         }
     }
