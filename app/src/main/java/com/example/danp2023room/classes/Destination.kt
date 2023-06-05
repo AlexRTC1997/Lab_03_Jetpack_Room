cbba51e21def2b1432b826a8f3466ddca40a118a
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-06-05 07:41:17 -0500

    FIX: Error at compile project

--- app/src/main/java/com/example/danp2023room/classes/Destination.kt
+++
@@ -1,9 +0,0 @@
-package com.example.danp2023room.classes
-
-sealed class Destination(val title: String, val route: String) {
-    object RoomSample : Destination("RoomSample", "roomSample")
-    object Register : Destination("Register","register")
-    object Students : Destination("Students","students")
-    object Courses : Destination("Courses","courses")
-    object Relationship : Destination("Relationship", "relationship")
-}
--- app/src/main/java/com/example/danp2023room/entities/CourseStudentCrossRef.kt
+++
@@ -1,9 +0,0 @@
-package com.example.danp2023room.entities
-
-import androidx.room.Entity
-
-@Entity(primaryKeys = ["courseId", "juniorId"])
-data class CourseStudentCrossRef(
-    val courseId: Int,
-    val juniorId: Int
-)
